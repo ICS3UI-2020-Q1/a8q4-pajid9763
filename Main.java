59168c6e8169addc007548d89722111c697142da
pajid9763 <pajid9763@wrdsb.ca>
2020-10-30 22:36:09 +0000

    not finished yet

--- Main.java
+++ Main.java
@@ -5,7 +5,14 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
-  
+  JPanel mainPanel;
+  JLabel nLabel;
+  JLabel facLabel;
+
+  JTextField nInput;
+  JTextField facInput;
+
+  JButton calcButton;
 
 
   // Method to assemble our GUI
@@ -18,6 +25,55 @@ public class Main implements Runnable, ActionListener{
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
+
+    // initialize the main JPanel 
+    mainPanel = new JPanel(); 
+    // turn on manual layouts 
+    mainPanel.setLayout(null);
+    // add the panel to the JFrame 
+    frame.add(mainPanel);
+
+
+    // create the labels
+    nLabel = new JLabel("n");
+    facLabel = new JLabel("factorial(n)");
+
+    // set the location and size of the labels 
+    nLabel.setBounds(30, 30, 30, 20);
+    facLabel.setBounds(150, 30, 80, 20);
+
+    // add the labels to the main panel 
+    mainPanel.add(nLabel);
+    mainPanel.add(facLabel);
+
+
+    // initialize the JTextFields 
+    nInput = new JTextField();
+    facInput = new JTextField();
+
+    // set the location and size of the JTextFields
+    nInput.setBounds(60, 30, 80, 20);
+    facInput.setBounds(235, 30, 100, 20);
+
+    // add the text fields to the main panel 
+    mainPanel.add(nInput);
+    mainPanel.add(facInput);
+
+
+    // initialize the button
+    calcButton = new JButton("CALC");
+
+    // add an actionListener 
+    calcButton.addActionListener(this);
+
+    // give the button an action command 
+    calcButton.setActionCommand("calc");
+
+    // set the location and size of the button 
+    calcButton.setBounds(340, 30, 70, 20);
+
+    // add button to the mainPanel 
+    mainPanel.add(calcButton);
  
     
 
@@ -28,7 +84,16 @@ public class Main implements Runnable, ActionListener{
     // get the command from the action
     String command = e.getActionCommand();
 
+    int total = 1;
+    String nInputText = nInput.getText;
+    double nInputNum = Double.parseDouble(nInputText);
+
+    for (int i = 1; i <= nInputNum; i++){
+      total = total * i;
+      String totalFinal = String.valueOf(total);
+    }
   }
+    facInput.setText(totalFinal);
 
   // Main method to start our program
   public static void main(String[] args){
